7aa1eb989ff0a382759ecce4d9854b1858baeec0
jamesbalashi <james.balashi@kevin.eu>
2022-10-22 17:57:38 +0300

    update

--- src/project/interfaces/ISections.ts
+++ src/project/interfaces/ISections.ts
@@ -1,6 +1,9 @@
+import {ContentTypeEnum} from '../../enums/ContentType.enum';
+
 export interface ISections {
-    title: string,
-    content: string,
+    title: string;
+    content: string;
     _id: number;
+    type: ContentTypeEnum;
     subSections: Array<ISections> | undefined;
 }
